311900b389d9392f11ba32273afa57e074082ea9
Gabriel Ross <gabriel.ross@outlook.com>
2023-03-15 17:00:29 -0400

    remove Currency enum in balances to eliminate redundant conversions

--- account.go
+++ account.go
@@ -4,9 +4,9 @@ import "time"
 
 // Account represents a trading account.
 type Account struct {
-	ID                string               `json:"id"`
-	Owner             string               `json:"owner"`
-	Balances          map[Currency]float64 `json:"balances"`
-	Reputation        int                  `json:"reputation"`
-	CreationTimestamp time.Time            `json:"creationTimestamp"`
+	ID                string             `json:"id"`
+	Owner             string             `json:"owner"`
+	Balances          map[string]float64 `json:"balances"`
+	Reputation        int                `json:"reputation"`
+	CreationTimestamp time.Time          `json:"creationTimestamp"`
 }
