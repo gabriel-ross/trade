df02640b2053d002105a45cc9189510d93e986d7
Gabriel Ross <gabriel.ross@outlook.com>
2023-03-16 11:51:47 -0400

    refactor rendering service into domain-service dependency for unittesting

--- render.go
+++ render.go
@@ -6,31 +6,25 @@ import (
 	"net/http"
 )
 
-func RenderJSON(w http.ResponseWriter, r *http.Request, statusCode int, body interface{}) {
+type RenderService struct{}
+
+func (rs *RenderService) RenderJSON(w http.ResponseWriter, r *http.Request, httpStatusCode int, body interface{}) {
 	out, err := json.MarshalIndent(body, "", "	")
 	if err != nil {
-		RenderError(w, r, err, http.StatusInternalServerError, "%s", err.Error())
+		rs.RenderError(w, r, err, http.StatusInternalServerError, "%s", err.Error())
 		return
 	}
 
-	w.WriteHeader(statusCode)
+	w.WriteHeader(httpStatusCode)
 	w.Write(out)
 }
 
-type errorResponse struct {
-	Err            error  `json:"-"`
-	HTTPStatusCode int    `json:"-"`
-	StatusText     string `json:"-"`
-	AppCode        int64  `json:"code,omitempty"`
-	ErrorText      string `json:"error,omitempty"`
-}
-
-func RenderError(w http.ResponseWriter, r *http.Request, svrErr error, code int, format string, args ...any) {
+func (rs *RenderService) RenderError(w http.ResponseWriter, r *http.Request, svrErr error, code int, format string, args ...any) {
 	var err error
-	errResp := newErrorResponse(code, svrErr, format, args...)
+	errResp := rs.newErrorResponse(code, svrErr, format, args...)
 	respBody, err := json.Marshal(errResp)
 	if err != nil {
-		mustWriteError(w, err)
+		rs.mustWriteError(w, err)
 		return
 	}
 
@@ -40,15 +34,23 @@ func RenderError(w http.ResponseWriter, r *http.Request, svrErr error, code int,
 	return
 }
 
-func newErrorResponse(code int, err error, format string, args ...any) *errorResponse {
+func (rs *RenderService) newErrorResponse(code int, err error, format string, args ...any) *errorResponse {
 	return &errorResponse{
 		Err:            err,
 		HTTPStatusCode: code,
 		ErrorText:      fmt.Sprintf(format, args...),
 	}
 }
-func mustWriteError(w http.ResponseWriter, err error) {
+func (rs *RenderService) mustWriteError(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte("error encountered while attempting to write error: " + err.Error()))
 	return
+}
+
+type errorResponse struct {
+	Err            error  `json:"-"`
+	HTTPStatusCode int    `json:"-"`
+	StatusText     string `json:"-"`
+	AppCode        int64  `json:"code,omitempty"`
+	ErrorText      string `json:"error,omitempty"`
 }
