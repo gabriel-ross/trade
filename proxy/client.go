c53a894b76b97df0b01e55bf766e789a12417436
Gabriel Ross <Gabriel.Ross@outlook.com>
2023-03-30 14:26:53 -0400

    some stuff

---
+++ proxy/client.go
@@ -0,0 +1,7 @@
+package proxy
+
+// ClientRequest represents a request made by Client
+type ClientRequest struct{}
+
+// ClientResponse represents a response to a request made by Client
+type ClientResponse struct{}
